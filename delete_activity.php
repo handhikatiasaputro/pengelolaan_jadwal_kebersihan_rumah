12d7327f45e679ac42790ccc095b490ffed1275e
HandhikaTiaSaputro <handikatiasaputra7900@gmail.com>
2024-10-18 15:18:26 +0700

    berhasil update

--- delete_activity.php
+++ delete_activity.php
@@ -1,15 +1,7 @@
 <?php
 require 'config.php';
+require "functions.php";
 
-// $id = $_GET['id'];
-
-// $sql = "DELETE FROM kegiatan WHERE id = $id";
-// if ($db->query($sql) === TRUE) {
-//     header("Location: index.php");
-//     exit();
-// } else {
-//     echo "Error deleting record: " . $db->error;
-// }
 handleFormSubmission($db);
 //mendelete
 deleteActivity($db, $id);
--- delete_member.php
+++ delete_member.php
@@ -2,15 +2,6 @@
 require 'config.php';
 require "functions.php";
 
-// $id = $_GET['id'];
-
-// $sql = "DELETE FROM member WHERE id = $id";
-// if ($db->query($sql) === TRUE) {
-//     header("Location: index.php");
-//     exit();
-// } else {
-//     echo "Error deleting record: " . $db->error;
-// }
-
+handleFormSubmission($db);
 deleteMember($db,$id);
 ?>
